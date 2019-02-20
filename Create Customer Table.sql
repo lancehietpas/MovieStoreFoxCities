a49a13e664f304866a7a9cb8f114c51622216af7
Lance Hietpas <lancehietpas@gmail.com>
2019-02-20 07:55:18 -0600

    Added file that created customer table

---
+++ Create Customer Table.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE customer (
+    customer_id INT,
+    store_id INT,
+    first_name VARCHAR(45),
+	last_name VARCHAR(45),
+	email VARCHAR(50),
+	address_id INT,
+	active INT,
+	create_date DATETIME,
+	last_update TIMESTAMP,
+
+); 
